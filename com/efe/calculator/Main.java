3fa7853b1394e42cdb0d8063624a3d29a17f554e
gokonat <aahmetefe@hotmail.com>
2026-07-16 16:45:13 -0400

    Added while loop and improved calculator flow

--- com/efe/calculator/Main.java
+++ com/efe/calculator/Main.java
@@ -18,44 +18,83 @@ public class Main {
 		Scanner scnr = new Scanner(System.in);
 		
 		Calculator calculate = new Calculator();
+	
 		
-		System.out.println("Type the numbers please (2 numbers at a time).");
-		int num1 = scnr.nextInt();
-		int num2 = scnr.nextInt();
+		boolean running = true;
 		
-		System.out.println("Type the calculation type you want , first digit will be taken for absVal.(sum, subtraction, multiplication, divide, absval and history).");
+	while (running) {
+		
+		System.out.println("Choose an operation (sum, subtraction, multiplication, divide, absval, history and exit):");
 		String type = scnr.next();
 		
-		if (type.equals("sum")) {
+		switch (type) {
+		
+		case "sum": {
+			
+			System.out.println("Type the 2 numbers that desired to sum");
+			int num1 = scnr.nextInt();
+			int num2 = scnr.nextInt();
 			System.out.println("Result of summation is: " + calculate.sum(num1, num2));
+			
+			break;
 		}
-		
-		else if (type.equals("history")) {
+		case "history":
 			calculate.showHistory();
-		}
 		
-		else if (type.equals("subtraction")) {
+			break;
+			
+		case "subtraction": {
+			
+			System.out.println("Type the 2 numbers that desired to subtract.");
+			int num1 = scnr.nextInt();
+			int num2 = scnr.nextInt();
 			System.out.println("Result of subtraction is: " + calculate.subtraction(num1, num2));
+			
+			break;
 		}
-		
-		else if (type.equals("multiplication")) {
+		case "multiplication": {
+			
+			System.out.println("Type the 2 numbers that desired to multiply.");
+			int num1 = scnr.nextInt();
+			int num2 = scnr.nextInt();
 			System.out.println("Result of multiplication is: " + calculate.multiplication(num1, num2));
+			
+			break;
 		}
-		
-		else if  (type.equals("divide")) {
+		case "divide": {
+			
+			System.out.println("Type the 2 numbers that desired to divide.");
+			int num1 = scnr.nextInt();
+			int num2 = scnr.nextInt();
+			
+			if (num2 != 0) {
 			System.out.println("Result of divide is: " + calculate.divide(num1, num2));
+			}
+			else {
+				System.out.println("Cant divide by zero!");
+			}
+			break;
 		}
-		
-		else if (type.equals("absval")) {
+		case "absval": {
+			
+			System.out.println("Type the number that desired to absolute value.");
+			int num1 = scnr.nextInt();
 			System.out.println("Result of absolute value of num is: " + calculate.absVal(num1));
+			
+			break;
 		}
 		
-		else {
-			System.out.println("Unknown type");
+		case "exit":
+			running = false;
+			break;
+		
+		default:
+			
+			System.out.println("Unknown type.");
 		}
 		
 		
-		
+	}
 		
 		
 		
